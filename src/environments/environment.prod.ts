fb723a0dc7353dec227267a6ca5eb1f586eeee13
Erik Villegas <erdvillegas@gmail.com>
2021-06-16 01:12:02 -0500

    First commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBzjwBE47CvfNISEr70mv-MOlkCdh7CT3M",
+    authDomain: "matylab-8e007.firebaseapp.com",
+    projectId: "matylab-8e007",
+    storageBucket: "matylab-8e007.appspot.com",
+    messagingSenderId: "252910589659",
+    appId: "1:252910589659:web:37b6b9b1ca128702f05efe",
+    measurementId: "G-TYY65X8CZD"
+  }
 };
