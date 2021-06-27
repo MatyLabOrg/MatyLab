89f6c85a65ea6e0f3788cbabac15ab418aebf748
Erik Villegas <erdvillegas@gmail.com>
2021-06-27 00:39:54 -0500

    Completando Recovery Pass

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { RecoveryComponent } from './recovery/recovery.component';
 
 const routes: Routes = [
-  { path:'', component:LoginComponent}
+  { path:'', component:LoginComponent},
+  { path:'recovery', component:RecoveryComponent},
 ];
 
 @NgModule({
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { AuthRoutingModule } from './auth-routing.module';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RecoveryComponent } from './recovery/recovery.component';
 
 
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    RecoveryComponent
   ],
   imports: [
     CommonModule,
     AuthRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule,
   ]
 })
 export class AuthModule { }
